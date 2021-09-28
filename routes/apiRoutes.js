debf280b65215b941b4421bd7e1b36e68e1ad200
ridalo <ridalo1989@gmail.com>
2021-09-28 01:14:54 -0400

    replaced arrow functions.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,36 +1,37 @@
 const fs = require("fs");
+const out_put = "./db/db.json";
 
-const output = "./db/db.json";
-const dataBase = require("../db/db.json")
-let id = dataBase.length + 1;
+const database = require("../db/db.json");
+let id = database.length + 1;
 
-module.exports = (app) => {
-    //create the api route
-    app.get("/api/notes", (req, res) => {
-        res.json(dataBase);
+module.exports = function (app) {
+  // Create the API Route
+  app.get("/api/notes", (req, res) => {
+    res.json(database);
+  });
+
+  // create a post for the API
+  app.post("/api/notes", (req, res) => {
+    req.body.id = id++;
+    database.push(req.body);
+    fs.writeFile(out_put, JSON.stringify(database), (err) => {
+      if (err) throw err;
     });
+    res.json(database);
+  });
 
-    //create a POST route for api
-    app.post("/api/notes"), (req, res)=>{
-        req.body.id = id++;
-        dataBase.push(req.body);
-        fs.writeFile(output, JSON.stringify(dataBase), (err)=>{
-            if(err) throw err;
-        });
-        res.json(dataBase)
-    };
+  // delete individual notes
+  app.delete("/api/notes/:id", (req, res) => {
+    let getId = req.params.id;
 
-    //delete individual notes
-    app.delete("/api/notes/:id", (req, res)=>{
-        let getId = req.params.id;
-        for (let i = 0; i < dataBase.length; i++) {
-            if (dataBase[i].id === parseInt(getId)){
-                dataBase.splice(i, 1)
-            }
-            fs.writeFile(output, JSON.stringify(dataBase), (err)=>{
-                if(err) throw err;
-            });
-            res.json(dataBase)
-        }
+    for (let i = 0; i < database.length; i++) {
+      if (database[i].id === parseInt(getId)) {
+        database.splice(i, 1);
+      }
+    }
+    fs.writeFile(out_put, JSON.stringify(database), function (err) {
+      if (err) throw err;
     });
+    res.json(database);
+  });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,10 @@
     const path = require ("path");
 
-module.exports = (app) => {
-    app.get("/", (req, res) => {
+module.exports = function (app) {
+    app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"))
     })
-    app.get("/notes", (req, res) => {
+    app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"))
     })
 }
